baa1c7f0a4ff5726c12030c4091eeb31ee7cb5e0
Jannaee Sick <jannaee@gmail.com>
2018-06-07 04:25:53 +0000

    chore: Setup code for enabling a python simple server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,8 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    /*const port = 1fab1f3bc2a3405a8012b7dee5333330.vfs.cloud9.us-east-2.amazonaws.com // Change this to your server port*/
+    return `http://1fab1f3bc2a3405a8012b7dee5333330.vfs.cloud9.us-east-2.amazonaws.com/data/restaurants.json`;
   }
 
   /**
