1e029d7ea79d574f4a6a43e7d2266471ded69582
Jannaee Sick <jannaee@gmail.com>
2018-06-07 23:42:47 +0000

    feat: Adding CSS grid templates and media queries Testing out grid templates and area for the first time.

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    /*const port = 8000 // Change this to your server port*/
+    /*const port = 8080 // Change this to your server port*/
     return `http://1fab1f3bc2a3405a8012b7dee5333330.vfs.cloud9.us-east-2.amazonaws.com/data/restaurants.json`;
   }
 
