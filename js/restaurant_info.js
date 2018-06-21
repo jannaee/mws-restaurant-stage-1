3d9a8d1aee7cfd230296b9fc205b4001ce97cdcd
Jannaee Sick <jannaee@gmail.com>
2018-06-20 22:46:49 -0400

    feat: Adding button for better accessibility

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiamFubmFlZSIsImEiOiJjamlseHhjcGYxcXEzM3BwZjlhcHJyejFoIn0.j9Dt94QduBtR4JZGunqlCQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
