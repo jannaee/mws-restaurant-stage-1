a25857200b0b39f0ba3f14828636619d91e317bc
Jannaee <jannaee@gmail.com>
2018-07-01 13:06:43 -0400

    refactor: Correcting service registration code

--- js/main.js
+++ js/main.js
@@ -219,15 +219,15 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 } 
 
 
-
-/* addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
-    // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-    google.maps.event.addListener(marker, 'click', () => {
-      window.location.href = marker.url
-    });
-    self.markers.push(marker);
-  });
-} */
+if ('serviceWorker' in navigator){
+  navigator.serviceWorker
+  .register('js/sw.js')
+  .then(
+    function(event){
+      console.log("We have registration!");
+    }, function(error){
+      console.log('Oh noo, registration failed');
+    }
+  )
+}
 
--- js/sw.js
+++ js/sw.js
@@ -1,22 +1,6 @@
 /**
  * Adding a service worker.
- */
-
- if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/js/sw.js').then(
-    function(serviceWorkerRegister) {
-    if(serviceWorkerRegister.installing) {
-      console.log('Installation in progress');
-    } else if(serviceWorkerRegister.waiting) {
-      console.log('Installation complete');
-    } else if(serviceWorkerRegister.active) {
-      console.log('Service Worker is Active');
-    }
-  }).catch(function(error) {
-    // registration failed
-    console.log('Uh oh! Failure detected: ' + error);
-  });
-}
+*/ 
 
 self.addEventListener('install', function(event){
   event.waitUntil(
