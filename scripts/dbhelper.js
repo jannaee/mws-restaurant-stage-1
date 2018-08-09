dc82db3de39f6d279cea43417bac7b730b5d1049
Jannaee Sick <jannaee@gmail.com>
2018-08-08 23:50:47 -0400

    refactor:Added Webp and imagemin node module for better performance

--- scripts/dbhelper.js
+++ scripts/dbhelper.js
@@ -188,7 +188,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`./img/${restaurant.photograph}.jpeg`);
+    return (`./img/${restaurant.photograph}.webp`);
 
   }
 
